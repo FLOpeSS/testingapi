1e7a0c3f948a161b95dcb4d510300745709d87e6
FLOpeSS <sneto4413@gmail.com>
2023-10-02 19:58:30 -0300

    updating albums endpoint

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # testingapi
 Rest api in golang
+
+
+Already have writted some endpoints calls
--- main.go
+++ main.go
@@ -7,23 +7,6 @@ import (
 	"net/http"
 )
 
-type context struct {
-	Request *http.Request
-	Writer  http.ResponseWriter
-	Albums  album
-}
-
-func (c *context) appendJson(status int, obj any) {
-	c.Writer.Header().Set("Content-Type", "applications/json")
-	response, err := json.Marshal(obj)
-	if err != nil {
-		fmt.Println("Error marshalling the program. Err: ", err)
-	}
-
-	c.Writer.WriteHeader(http.StatusOK)
-	c.Writer.Write(response)
-}
-
 type album struct {
 	ID     string  `json:"id"`
 	Title  string  `json:"title"`
@@ -37,44 +20,40 @@ var albums = []album{
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
-// func appendJson(w http.ResponseWriter, alb album) {
-// 	w.Header().Set("Content-Type", "applications/json")
-// 	response, err := json.Marshal(alb)
-// 	if err != nil {
-// 		fmt.Println("Error marshalling the program. Err: ", err)
-// 	}
-//
-// 	w.WriteHeader(http.StatusOK)
-// 	w.Write(response)
-// }
-
-func getAlbums(w http.ResponseWriter, r *http.Request) {
-	var con *context
-	con.appendJson(http.StatusCreated, albums)
+func createJson(w http.ResponseWriter, status int, obj any) {
+	w.Header().Set("Content-type", "applications/json")
+	response, err := json.MarshalIndent(obj, "", " ")
+	if err != nil {
+		fmt.Println("Error marshalling the program. Err: ", err)
+	}
+	w.WriteHeader(status)
+	w.Write(response)
 }
 
-func postAlbums(w http.ResponseWriter, r *http.Request, c *context) {
+func requestAlbums(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
+		var newAlbums album
 		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
 			fmt.Println("Error reading the body request. Error: ", err)
 		}
 
-		var newAlbums album
 		json.Unmarshal(body, &newAlbums)
 
-		// albums := append(albums, newAlbums)
-		// getAlbums(http.StatusCreated, newAlbums)
-		c.appendJson(http.StatusCreated, newAlbums)
+		albums = append(albums, newAlbums)
+		createJson(w, http.StatusCreated, newAlbums)
+	}
 
+	if r.Method == http.MethodGet {
+		createJson(w, http.StatusCreated, albums)
 	}
 }
 
 func main() {
+	PORT := ":8000"
+	http.HandleFunc("/albums", requestAlbums)
 
-	http.HandleFunc("/albums", getAlbums)
-
-	err := http.ListenAndServe(":8000", nil)
+	err := http.ListenAndServe(PORT, nil)
 	if err != nil {
 		fmt.Println(err)
 	}
