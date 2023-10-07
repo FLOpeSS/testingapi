b04cb3b6fccd55d8abf597701ae199cfdfe2f593
FLOpeSS <sneto4413@gmail.com>
2023-10-07 15:02:09 -0300

    updating route

--- main.go
+++ main.go
@@ -14,13 +14,16 @@ type album struct {
 	Price  float64 `json:"price"`
 }
 
+type Hash map[string]any
+
 var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+	{ID: "4", Title: "Rethoric", Artist: "Aristoteles", Price: 5000},
 }
 
-func createJson(w http.ResponseWriter, status int, obj any) {
+func creating(w http.ResponseWriter, status int, obj any) {
 	w.Header().Set("Content-type", "applications/json")
 	response, err := json.MarshalIndent(obj, "", " ")
 	if err != nil {
@@ -28,6 +31,20 @@ func createJson(w http.ResponseWriter, status int, obj any) {
 	}
 	w.WriteHeader(status)
 	w.Write(response)
+}
+
+func createJson(w http.ResponseWriter, status int, obj any) {
+	w.Header().Set("Content-type", "applications/json")
+	response, err := json.MarshalIndent(obj, "", " ")
+	// response, err := json.Marshal(obj)
+	if err != nil {
+		fmt.Println("Error marshalling the program. Err: ", err)
+	}
+	w.WriteHeader(status)
+	w.Write(response)
+}
+
+func testing_something_new() {
 }
 
 func requestAlbums(w http.ResponseWriter, r *http.Request) {
@@ -49,12 +66,29 @@ func requestAlbums(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func get_album_by_id(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+	}
+	r.ParseForm()
+	id := r.FormValue("id")
+	for _, a := range albums {
+		if a.ID == id {
+			createJson(w, http.StatusOK, a)
+			return
+		}
+	}
+	createJson(w, http.StatusNotFound, "not found")
+}
+
 func main() {
 	PORT := ":8000"
 	http.HandleFunc("/albums", requestAlbums)
+	http.HandleFunc("/albums:id", requestAlbums)
+	// http.HandleFunc("/albums", get_album)
 
 	err := http.ListenAndServe(PORT, nil)
 	if err != nil {
 		fmt.Println(err)
 	}
+
 }
